b7e4ad810a2fc6379bf945bd021c062c881dc8df
Daniel Cazabat <dacazabat@gmail.com>
2023-01-16 20:34:30 -0300

    Loading - Footer

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -2,7 +2,11 @@ import './Loading.css'
 
 export default function Loading() {
   return (
-    <div className='load'>
+    <div className="loading">
+      <div className="loading__spinner"></div>
     </div>
   )
 }
+
+
+// Create functional reactjs component loading with CSS animation
